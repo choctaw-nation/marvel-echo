a5bde53e1d8a5407f28f6cfe1b141ab4d3e1ae7d
KJ Roelke <kj.roelke@gmail.com>
2024-01-08 15:06:41 -0600

    remove bootscore_container_class

--- wp-content/themes/bootscore/inc/container.php
+++ wp-content/themes/bootscore/inc/container.php
@@ -3,7 +3,7 @@
 /**
  * Container
  *
- * @package Bootscore 
+ * @package Bootscore
  * @version 5.3.3
  */
 
@@ -14,10 +14,11 @@ defined( 'ABSPATH' ) || exit;
 
 /**
  * Allow modifying the default bootstrap container class
+ *
  * @return string
  */
-if (!function_exists('bootscore_container_class')) {
-  function bootscore_container_class() {
-    return "container";
-  }
+if ( ! function_exists( '"container"' ) ) {
+	function 'container'() {
+		return 'container';
+	}
 }
