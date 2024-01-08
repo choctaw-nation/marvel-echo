20d06bc56f28427c07b490b5489ea89b196cea40
KJ Roelke <kj.roelke@gmail.com>
2024-01-08 12:40:02 -0600

    swap em dash for hyphen

--- wp-content/themes/bootscore/template-parts/sections/aside-director-quote.php
+++ wp-content/themes/bootscore/template-parts/sections/aside-director-quote.php
@@ -26,7 +26,7 @@
 				</div>
 				<div class="col-12 col-lg-7 pullout-text">
 					<p class="mb-5">"It's so exciting to be able to premiere Echo and it was extremely meaningful to myself to have this screening in Choctaw Nation. One of the things we're most excited about is being able to portray the Choctaw culture hopefully in an authentic and exciting way."</p>
-					<span class="h3" data-aos="fade-down">&mdash; Sydney Freeland, Director</span>
+					<span class="h3" data-aos="fade-down">- Sydney Freeland, Director</span>
 					<button type="button" class="btn btn-easter-egg" data-bs-toggle="modal" data-bs-target="#bowModal">
 						<i class="fa-kit fa-choctaw-echo-bow text-light"></i>
 					</button>
